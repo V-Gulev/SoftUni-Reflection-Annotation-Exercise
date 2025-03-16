c9d7c7d4c26f17fc2ac62e5050ee6d7a39b5dcc3
V-Gulev <velizargulev@abv.bg>
2025-03-16 18:10:12 +0200

    Harvesting Fields 01

--- harvestingFields/Main.java
+++ harvestingFields/Main.java
@@ -1,7 +1,34 @@
 package harvestingFields;
 
-public class Main {
-	public static void main(String[] args) {
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
 
-	}
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        Field[] fields = RichSoilLand.class.getDeclaredFields();
+        Consumer<Field> printer = field -> System.out.println(String.format("%s %s %s",
+                        Modifier.toString(field.getModifiers()),
+                        field.getType().getSimpleName(),
+                        field.getName()));
+
+        while (!input.equals("HARVEST")) {
+            String finalInput = input;
+            Field[] requestedFields = Arrays.stream(fields)
+                    .filter(field -> Modifier.toString(field.getModifiers()).equals(finalInput)).toArray(Field[]::new);
+
+            if (requestedFields.length != 0) {
+                Arrays.stream(requestedFields).forEach(printer);
+            } else {
+                Arrays.stream(fields).forEach(printer);
+            }
+
+            input = scanner.nextLine();
+        }
+
+    }
 }
